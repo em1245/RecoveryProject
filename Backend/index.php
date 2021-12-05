7bd541a5f1d16a007f3a270771b3dec8616ff137
davidita118 <david.isaac.iker@hotmail.com>
2021-12-05 17:40:55 -0600

    feat: lista de peticiones

---
+++ Backend/index.php
@@ -0,0 +1,46 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST");
+header('Content-Type: application/json');
+
+require_once './Backend/app/Model/SolrModel.php';
+require_once './Backend/app/Model/GrammarModel.php';
+require_once './Backend/app/Model/ConnectionModel.php';
+
+if(isset($_GET['crawler'])) {
+    $url = $_GET['crawler'];
+    $level = $_GET['level'] ? $_GET['level'] : 0;
+
+    $response = postURL($url, $level, $name_core);
+
+    echo json_encode(["msg" => $response]);
+}
+
+if(isset($_GET['create_core'])) {
+    $response = createCore();
+
+    echo json_encode(["message" => $response]);
+}
+
+if(isset($_GET['search'])) {
+    $search = $_GET['search'];
+    $facet = $_GET['facet'] ? true : false;
+
+    $response = getQuery($search, $facet);
+
+    echo $response;
+}
+
+if(isset($_GET['grammar'])) {
+    $grammar = $_GET['grammar'];
+
+    if(isset($_GET['v'])) {
+        $v = $_GET['v'];
+        $response = getGrammar($grammar, $v);
+    }else {
+        $response = getGrammar($grammar);
+    }
+
+    echo $response;
+}
