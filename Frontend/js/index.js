96e30eee2a8f71565dbd38f09cef50a63ce92333
Ariel May <aemd199711@gmail.com>
2021-12-06 17:58:30 -0600

    feat: indexar páginas del textarea

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,9 +1,44 @@
 const formSearch = document.getElementById("form-search");
 const inputSearch = document.getElementById("input-search");
+const results = document.getElementById("results");
+const pages = document.getElementById("pages");
+const checkbox =  document.getElementById("faceted");
+const index = document.getElementById("form-index");
+const urls = document.getElementById("urls");
+
 
 inputSearch.value = "hola AND casa OR uady mate NOT pluma";
 const operators = ["or", "and", "not"];
-let globalQuery = "";
+let faceted = false
+checkbox.checked = faceted;
+
+checkbox.addEventListener("click", ()=>{
+  checkbox.checked = !faceted;
+  faceted = !faceted;
+})
+
+index.addEventListener("submit", (event)=>{
+  event.preventDefault();
+  const values = urls.value.split("\n");
+  const promises = values.map(async value=>{
+    return await indexUrl(value);
+  })
+
+  Promise.all(promises).then((query) => {
+    console.log(query)
+  });
+})
+
+const indexUrl = async (url) => {
+  const enpoint = `http://localhost:3000/index.php?crawler=${url}`;
+  try {
+    const result = await fetch(enpoint);
+    return await result.json();
+  } catch (error) {
+    console.error(error);
+    return {}
+  }
+}
 
 formSearch.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -33,15 +68,45 @@ formSearch.addEventListener("submit", (event) => {
   });
 
   Promise.all(query).then((query) => {
-    console.log(query.join(" "));
+    searchDocuments(query.join(" "));
   });
 });
 
-const requestPages = (query) => {};
+const searchDocuments = async (query) => {
+  try {
+    const url = `http://localhost:3000/index.php?search=${query}&&faceted=${faceted}`;
+    const result = await fetch(url);
+    const response = await result.json();
+    renderDocuments(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const renderDocuments = (data) => {
+  results.innerHTML = `Resultados encontrados ${data.response.numFound}`;
+  console.log(data);
+  let html = `<div class='item hd'>
+                <p class="id hd">Enlace</p>
+                <p class="title hd">Titulo</p>
+                <p class="desc hd">Descripción</p>
+                <p class="score hd">Score</p>
+              </div>`;
+
+  data.response.docs.map(doc=>{
+    html += "<div class='item'>"
+    html += `   <a class="id" href=${doc.id}>Enlace</a>`
+    html += `   <p class="title">${doc.title}</´p>`
+    html += `   <p class="desc">${doc.description}</p>`
+    html += `   <p class="score">${doc.score.toFixed(4)}</p>`
+    html += "</div>"
+  });
+
+  pages.innerHTML = html;  
+}
 
 const createQuery = (words, op) => {
   const query = words.map((w) => `${op}${w.word}`);
-  console.log(query.join(" "));
   return query.join(" ");
 };
 
